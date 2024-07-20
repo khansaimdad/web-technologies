c2b85df50502013bf00964afbf3616cf5078cfa1
Khansa Imdad <khansaimdad38@gmail.com>
2024-07-20 12:50:52 +0500

    Delete Lab 3 directory

--- Lab 3/All tasks.py
+++
@@ -1,4 +0,0 @@
-#task 1
-# for num in range(1500, 2701):
-    if num % 7 == 0 and num % 5 == 0:
-        print(num)
--- Lab 3/task5.py
+++
@@ -1,6 +0,0 @@
-word = input("Enter a word: ")
-reverse = ''
-
-for i in range(len(word) - 1, -1, -1):
-    reverse += word[i]
-print("Reverse of the word is: ", reverse)
